304b8983e3fa368ff53bb039d9ee65e09a2c057d
Spade <jameschubbuck@hunschool.org>
2023-04-16 14:31:28 -0400

    Things?

--- speech-to-text.js
+++ speech-to-text.js
@@ -1,24 +1,9 @@
+recognition = new webkitSpeechRecognition()
+
+recognition.onresult = function (event) {
+    speak(event.results[0][0].transcript);
+}
+
 window.onload = function() {
-    recognition = new webkitSpeechRecognition()
-    recognition.interimResults = true
-    recognition.continuous = true
     recognition.start()
-    recognition.onresult = function (event) {
-        for (var i = event.resultIndex; i < event.results.length; i++) {
-            for(var j = 0; j <  event.results[i].length;j++){
-                console.log(event.results[i][j].transcript)
-            }
-        }
-    }
-    recognition.onerror = function (event) {
-        console.log(event)
-    }
-    recognition.onend = function (event) {
-        console.log("end")
-        console.log(recognition)
-        recognition.start()
-    }
-    recognition.onstart = function (event){
-        console.log("start")
-    }
 }
